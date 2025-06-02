08b990ec968d257fef784737ade7bde5a7b866cf
Douglas-Oliveira-silva <douglasoliveira.dasilva@hotmail.com>
2025-06-02 20:24:24 -0300

    feat: finalizando FoodController com as anotações swagger

--- src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
+++ src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
@@ -5,10 +5,14 @@ import dev.java10x.MagicFridgeAI.dto.FoodDTO;
 import dev.java10x.MagicFridgeAI.model.FoodItem;
 import dev.java10x.MagicFridgeAI.repository.FoodItemRepository;
 import dev.java10x.MagicFridgeAI.service.FooditemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
-import org.hibernate.annotations.Parameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,17 +30,36 @@ public class FoodItemController {
     }
 
   // 1. CRIAR
+@PostMapping("/criar")
+@Operation(summary = "Criar comidas", description = "Essa rota cria novas comidas para a geladeira!")
+@ApiResponses({
+        @ApiResponse(responseCode = "201", description = "Comida criada com sucesso!"),
+        @ApiResponse(responseCode = "400", description = "erro na criação da comida!"),
+        @ApiResponse(responseCode = "500", description = "Erro na criação da comida, alguma chave foi violada!")
+})
 public ResponseEntity<String> criarComida(@RequestBody @Valid FoodDTO food){
         FoodDTO novaComida = fooditemService.criarComida(food);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body("Comida Criada com sucesso + " novaComida.getNome() + "(id) " + novaComida.getId() );
+                .body("Comida Criada com sucesso  " + novaComida.getNome() + "(id) " + novaComida.getId() );
 }
  // 2. LISTAR
-    public ResponseEntity<List<FoodDTO> listarComida(){
+    @GetMapping("/listar")
+    @Operation(summary = "Listar comidas", description = "essa rota lista as comidas que estão na geladeira")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Comidas listadas com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "erro para listar as comidas")
+    })
+    public ResponseEntity<List<FoodDTO>>listarComida(){
         List<FoodDTO> comida = fooditemService.ListarComida();
         return ResponseEntity.ok(comida);
     }
  // 3. LISTAR POR ID
+    @GetMapping("/listar/{id}")
+    @Operation(summary = "Lista comidas por id", description = "essa rota lista as comidas pelo seu Id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Comida encontrada com sucesso!"),
+            @ApiResponse(responseCode = "400", description = "comida não encontrada")
+    })
     public ResponseEntity<?> ListasComidaPorId(@PathVariable long id){
         FoodDTO comida = fooditemService.ListarComidaPorId(id);
 
@@ -47,7 +70,39 @@ public ResponseEntity<String> criarComida(@RequestBody @Valid FoodDTO food){
                     .body("Carro co o ID: " + id + "Não existe nos nossos registro");
         }
     }
-
     // 4. ALTERAR
-    
+    @PutMapping("alterar/{id}")
+    @Operation(summary = "Altera uma comida pelo Id", description = "Essa rota altera uma comida pelo seu Id")
+    @ApiResponses(value = {
+            @ApiResponse (responseCode = "200", description = "Comida alterada com sucesso!"),
+            @ApiResponse (responseCode = "400", description = "comida não encontrada, não foi possível alterar.")
+    })
+    public ResponseEntity<?> alterarComidaPorId(
+            @Parameter(description = "usuario manda o id no caminho da requisição")
+            @PathVariable long id,
+            @Parameter(description = "usuario manda os dados da comida para ser atualizada no corpo da requisição ")
+            @RequestBody FoodDTO comidaAtualizada){
+
+        FoodDTO alterarComida = fooditemService.Atualizarcomida(id, comidaAtualizada);
+
+        if (alterarComida != null){
+            return ResponseEntity.ok(alterarComida);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Comida com o Id " + id + " Não existe!");
+        }
+    }
+
+    // 5. Deletar
+
+    public ResponseEntity<String> deletarComidaPorId(@PathVariable long id){
+
+        if(fooditemService.ListarComidaPorId(id) != null){
+          fooditemService.deletarComidaporId(id);
+          return ResponseEntity.ok("Comida com o Id " + id + " deletada com sucesso!");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("a comida com o id " + id + " nao foi encontrada!");
+        }
+    }
+
+
 }
