804c957bd6cf06c3f2af028a74d1ecc4e19ed3e1
Douglas Oliveira da silva <douglasoliveira.dasilva@hotmail.com>
2025-06-02 16:14:41 -0300

    feat: adiciona funcções no controller, ainda faltando adicionar: alterar por id/deletar - adicionar os endpoints e anotations

--- src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
+++ src/main/java/dev/java10x/MagicFridgeAI/controller/FoodItemController.java
@@ -1,16 +1,19 @@
 package dev.java10x.MagicFridgeAI.controller;
 
 
+import dev.java10x.MagicFridgeAI.dto.FoodDTO;
 import dev.java10x.MagicFridgeAI.model.FoodItem;
 import dev.java10x.MagicFridgeAI.repository.FoodItemRepository;
 import dev.java10x.MagicFridgeAI.service.FooditemService;
+import jakarta.validation.Valid;
+import org.hibernate.annotations.Parameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @RestController
 @RequestMapping("/food")
@@ -22,19 +25,29 @@ public class FoodItemController {
         this.fooditemService = fooditemService;
     }
 
-    //POST
-    public ResponseEntity<FoodItem> criar(@RequestBody FoodItem foodItem){
-        FoodItem salvo = fooditemService.salvar(foodItem);
-        return ResponseEntity.ok(salvo);
+  // 1. CRIAR
+public ResponseEntity<String> criarComida(@RequestBody @Valid FoodDTO food){
+        FoodDTO novaComida = fooditemService.criarComida(food);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Comida Criada com sucesso + " novaComida.getNome() + "(id) " + novaComida.getId() );
+}
+ // 2. LISTAR
+    public ResponseEntity<List<FoodDTO> listarComida(){
+        List<FoodDTO> comida = fooditemService.ListarComida();
+        return ResponseEntity.ok(comida);
+    }
+ // 3. LISTAR POR ID
+    public ResponseEntity<?> ListasComidaPorId(@PathVariable long id){
+        FoodDTO comida = fooditemService.ListarComidaPorId(id);
+
+        if (comida != null){
+            return ResponseEntity.ok(comida);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Carro co o ID: " + id + "Não existe nos nossos registro");
+        }
     }
 
-    //GET
-
-
-    //UPDATE
-
-    //DELETE
-
-
-
+    // 4. ALTERAR
+    
 }
